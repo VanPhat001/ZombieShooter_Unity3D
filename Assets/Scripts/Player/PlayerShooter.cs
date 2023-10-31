3fb169ac9c7c197aa578708fd6ea2aaad5c5335e
VanPhat <vanphatct2002@gmail.com>
2023-11-01 01:24:11 +0700

    commit

--- Assets/Scripts/Player/PlayerShooter.cs
+++ Assets/Scripts/Player/PlayerShooter.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerShooter : MonoBehaviour
 {
+    float timeBetweenShoots = 0.2f;
+    float tick = 0;
+
     public void ShootObject()
     {
 
@@ -11,16 +14,44 @@ public class PlayerShooter : MonoBehaviour
 
     public void ShootZombie()
     {
-        
+
     }
 
     void Shoot()
     {
+        GameObject cam = PlayerController.Instance.fpsCamera;
+        RaycastHit hit;
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out hit))
+        {
+            if (hit.transform.tag.StartsWith("Object"))
+            {
+                ShootObject();
+            }
+            else if (hit.transform.tag.Equals("Zombie"))
+            {
+                ShootZombie();
+            }
+        }
+    }
 
+    void ThrowGrenade()
+    {
+        
     }
 
     private void Update()
     {
-        Shoot();
+        this.tick += Time.deltaTime;
+        if (Input.GetMouseButton(0) && this.tick >= this.timeBetweenShoots)
+        {
+            if (PlayerController.Instance.useGun)
+            {
+                Shoot();
+            }
+            else
+            {
+                ThrowGrenade();
+            }
+        }
     }
 }
